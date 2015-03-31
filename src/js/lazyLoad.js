fdece0a4ae0df6b3ac6471ccd2c5e384270c10fc
wangchi <hiwangchi@gmail.com>
2015-03-31 15:12:54 +0800

    add lazyLoad base test

---
+++ src/js/lazyLoad.js
@@ -0,0 +1,54 @@
+/**
+* lazyLoad v0.1.0
+* 图片延迟加载插件(配合jQuery或Zepto使用)
+*
+* @WalkerBe http://www.walkerbe.com/
+* MIT
+*/
+
+var lazyLoad = (function () {
+
+    var screenHeight = $(window).height();
+    var scrollHeight = 0;
+    var body = $('body');
+
+    /**
+     * 传入图片元素，执行加载
+     * @param imgs
+     */
+    function doLoad ( imgs ) {
+        for ( var i = 0, len = imgs.length; i < len; i++) {
+            loadImg($(imgs[i]));
+            scrollLoad($(imgs[i]));
+        }
+    }
+
+    /**
+     * 滚动时重新载入图片加载函数
+     * @param img
+     */
+    function scrollLoad ( img ) {
+        $(window).scroll(function () {
+            loadImg(img);
+        });
+    }
+
+    /**
+     * 图片加载函数
+     * @param img
+     */
+    function loadImg ( img ) {
+        scrollHeight = body.scrollTop();
+        if (scrollHeight + screenHeight > img.offset().top) {
+            if ( img.attr('data-src') == 'loaded' ) return;
+            img.attr('src', img.attr('data-src'));
+            img.attr('data-src', 'loaded');
+            console.log(img.attr('src') + ' -- Is Loaded');
+        }
+    }
+
+    return {
+      doLoad: doLoad
+    };
+
+})();
